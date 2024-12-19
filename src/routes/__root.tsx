8e3e53061f7528dd2fccfde91d6cad3f1652ed0a
Juan gomen <gomen98.jg@gmail.com>
2024-12-19 02:29:52 -0600

    feat: add loader and error component for root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,12 +6,18 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import { Toaster } from "@/components/ui/toaster";
+import { Loader } from "@/components/Loader";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
 }>()({
   component: RootComponent,
+  pendingComponent: Loader,
   notFoundComponent: PageNotFound,
+  errorComponent: (error) => {
+    console.error(error);
+    return <div>Error</div>;
+  },
 });
 
 function RootComponent() {
